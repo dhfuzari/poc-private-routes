f8b152651dc2a1b1070359d739bf4a1b59de6ce3
Daniel Henrique Fuzari <dhfuzari@gmail.com>
2020-01-09 17:54:30 -0300

    feat(pages): login page created

---
+++ src/pages/login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Login({ history }) {
+  function handleLogin() {
+    localStorage.setItem('@SuaAplicacao:JWT_TOKEN', 'seutokenjwt');
+    history.push('Main');
+  }
+  return (
+    <form onSubmit={handleLogin}>
+      <input type="text" name="email" placeholder="Seu e-mail" />
+      <input type="password" name="password" placeholder="Sua senha" required />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
